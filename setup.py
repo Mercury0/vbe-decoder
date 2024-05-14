931ec784a335f350fea5820ed3a34599528c2469
Mercury0 <mfox05@gmail.com>
2024-05-14 07:58:12 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vbe-decoder',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vbe-decoder=vbe_decoder:main',
+        ],
+    },
+    description='Decode an encoded VBScript, often seen as a .vbe file',
+    author='John Hammond',
+    url='https://github.com/Mercury0/vbe-decoder',
+)
